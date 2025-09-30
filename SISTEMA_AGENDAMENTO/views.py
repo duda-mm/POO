f59d6a7ef0412d0cefab5fc1a683ced0a0f8a011
Duda M <dudamdemedeiros@gmail.com>
2025-09-30 13:13:28 +0000

    profissional concluido

--- SISTEMA_AGENDAMENTO/views.py
+++ SISTEMA_AGENDAMENTO/views.py
@@ -1,6 +1,7 @@
 from models.cliente import Cliente, ClienteDAO
 from models.servico import Servico, ServicoDAO
 from models.horario import Horario, HorarioDAO
+from models.profissional import Profissional, ProfissionalDAO
 
 class View:
     def cliente_listar():
@@ -39,23 +40,43 @@ class View:
         servico = Servico(id, "0", 1)
         ServicoDAO.excluir(servico)
 
-    def horario_inserir(data, confirmado, id_cliente, id_servico):
+    def horario_inserir(data, confirmado, id_cliente, id_servico, id_profissional):
         c = Horario(0, data)
         c.set_confirmado(confirmado)
         c.set_id_cliente(id_cliente)
         c.set_id_servico(id_servico)
+        c.set_id_profissional(id_profissional)
         HorarioDAO.inserir(c)
 
     def horario_listar():
         return HorarioDAO.listar()
 
-    def horario_atualizar(id, data, confirmado, id_cliente, id_servico):
+    def horario_atualizar(id, data, confirmado, id_cliente, id_servico, id_profissional):
         c = Horario(id, data)
         c.set_confirmado(confirmado)
         c.set_id_cliente(id_cliente)
         c.set_id_servico(id_servico)
+        c.set_id_profissional(id_profissional)
         HorarioDAO.atualizar(c)
 
     def horario_excluir(id):
         c = Horario(id, None)
         HorarioDAO.excluir(c)
+
+    def profissional_listar():
+        return ProfissionalDAO.listar()
+    
+    def profissional_listar_id(id):
+        return ProfissionalDAO.listar_id(id)
+    
+    def profissional_inserir(nome, especialidade, conselho):
+        profissional = Profissional(0, nome, especialidade, conselho)
+        ProfissionalDAO.inserir(profissional)
+
+    def profissional_atualizar(id, nome, especialidade, conselho):
+        profissional = Profissional(id, nome, especialidade, conselho)
+        ProfissionalDAO.atualizar(profissional)
+        
+    def profissional_excluir(id):
+        profissional = Profissional(id, "", "", "")
+        ProfissionalDAO.excluir(profissional)
